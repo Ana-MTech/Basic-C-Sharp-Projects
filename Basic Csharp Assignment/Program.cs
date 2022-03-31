118d433ea757669269945be3a59675dcae64f87e
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-03-31 22:14:19 +0100

    Add files via upload

---
+++ Basic Csharp Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Rememer, we're \"accounting\" on you!");
+                         
+            Console.ReadLine();
+
+        }
+    }
+
+
+
