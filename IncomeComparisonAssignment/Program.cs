a7cea1d52c0a082c1013a7d2c98ad98173d41250
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-04-02 17:27:11 +0100

    Add files via upload

--- IncomeComparisonAssignment/Program.cs
+++ IncomeComparisonAssignment/Program.cs
@@ -17,39 +17,78 @@ namespace IncomeComparisonAssignment
             Console.WriteLine("Person 1");
             Console.WriteLine("");
 
-            Console.WriteLine("Hourly rate?");
-            Console.WriteLine("15");
+            int hourlyRate1 = 0;
+            Console.WriteLine("Haurly rate?");
+            hourlyRate1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("");
 
+            int hoursWork1 = 0;
             Console.WriteLine("Hours worked per week?");
-            Console.WriteLine("40");
+            hoursWork1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("");
 
             Console.WriteLine("Person 2");
             Console.WriteLine("");
 
-            Console.WriteLine("Hourly rate?");
-            Console.WriteLine("20");
+            int hourlyRate2 = 0;
+            Console.WriteLine("Haurly rate?");
+            hourlyRate2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("");
 
+            int hoursWork2 = 0;
             Console.WriteLine("Hours worked per week?");
-            Console.WriteLine("40");
+            hoursWork2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("");
 
             int weeksInyear = 52;
-            int hWork = 40;
-            int hourlyRate1 = 15;
-            int hourlyRate2 = 20;
 
             Console.WriteLine("Annual salary of Person 1");
-            int annualSalary1 = hourlyRate1 * hWork * weeksInyear;
+            int annualSalary1 = hourlyRate1 * hoursWork1 * weeksInyear;
             Console.WriteLine(annualSalary1);
             Console.WriteLine("");
 
             Console.WriteLine("Annual salary of Person 2");
-            int annualSalary2 = hourlyRate2 * hWork * weeksInyear;
+            int annualSalary2 = hourlyRate2 * hoursWork2 * weeksInyear;
             Console.WriteLine(annualSalary2);
             Console.WriteLine("");
+
+
+
+
+
+            //Console.WriteLine("Hourly rate?");
+            //Console.WriteLine("15");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Hours worked per week?");
+            //Console.WriteLine("40");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Person 2");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Hourly rate?");
+            //Console.WriteLine("20");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Hours worked per week?");
+            //Console.WriteLine("40");
+            //Console.WriteLine("");
+
+            //int weeksInyear = 52;
+            //int hWork = 40;
+            //int hourlyRate1 = 15;
+            //int hourlyRate2 = 20;
+
+            //Console.WriteLine("Annual salary of Person 1");
+            //int annualSalary1 = hourlyRate1 * hWork * weeksInyear;
+            //Console.WriteLine(annualSalary1);
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Annual salary of Person 2");
+            //int annualSalary2 = hourlyRate2 * hWork * weeksInyear;
+            //Console.WriteLine(annualSalary2);
+            //Console.WriteLine("");
 
             Console.WriteLine("Person 1 makes more money than Person 2");
             bool trueOrfalse = annualSalary1 > annualSalary2;
