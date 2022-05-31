2671c207cb0f7bb1ea942b3158e648db8e956e06
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-05-31 01:04:34 +0100

    Add files via upload

--- ArraysAssignment1S/ArraysAssignment1/Program.cs
+++ ArraysAssignment1S/ArraysAssignment1/Program.cs
@@ -13,14 +13,7 @@ class Program
         Console.WriteLine("");
 
         string[] Books = { "1. The Lord of the Rings", "2. The Silmarilliom", "3. The Fellowship of the Ring", "4. The Hobbit", "5. The Two Towers", "6. The Fall of Gondolin" };
-        //string[] Books = new string[6];
-        //Books[0] = "1.The Lord of the Rings";
-        //Books[1] = "2.The Silmarilliom";
-        //Books[2] = "3.The Fellowship of the Ring";
-        //Books[3] = "4.The Hobbit";
-        //Books[4] = "5.The Two Towers";
-        //Books[5] = "6.The Fall of Gondolin";
-
+    
         Console.WriteLine(Books[0]);
         Console.WriteLine(Books[1]);
         Console.WriteLine(Books[2]);
@@ -28,43 +21,58 @@ class Program
         Console.WriteLine(Books[4]);
         Console.WriteLine(Books[5]);
         Console.WriteLine("");
-
-        //string[] Books = new string[6];
-        //Books[0] = "The Lord of the Rings";
-        //Books[1] = "The Silmarilliom";
-        //Books[2] = "The Fellowship of the Ring";
-        //Books[3] = "The Hobbit";
-        //Books[4] = "The Two Towers";
-        //Books[5] = "The Fall of Gondolin";
-
-        //int packageWeight = 0;
-        //Console.WriteLine("Please enter the package weight.");
-        //packageWeight = Convert.ToInt32(Console.ReadLine());
-        //Console.WriteLine("");
-
+       
         int BookNumber = 0;
-        BookNumber = Convert.ToInt32(Console.ReadLine());
+        BookNumber = Convert.ToInt32(Console.ReadLine())-1;
+        Console.WriteLine(Books[BookNumber]);
         Console.WriteLine("");
 
-        ////packageWeight = Convert.ToInt32(Console.ReadLine());
-        //Console.ReadLine();
 
+        Console.WriteLine("We have 4 book clubs, choose one that best suite your time table...All club meet at our Library but in different times and days");
+        Console.WriteLine("");
+        Console.WriteLine("Choose the one that best siute you by choosing the corresponding number");
+        Console.WriteLine("");
+        Console.WriteLine("1. at 9am every Saturday, 2. 10.30am on Saturday, 3. 5pm on Friday, 4. at 5pm on Tuesday");
+        Console.WriteLine("");
+       
+        int[] Times = { 1, 2, 3, 4 };
 
+        //Console.WriteLine(Times[0]);
+        //Console.WriteLine(Times[1]);
+        //Console.WriteLine(Times[2]);
+        //Console.WriteLine(Times[3]);
+        //Console.WriteLine("");
 
-        //Console.WriteLine(n2[5]);
-        //Console.ReadLine();
+        int MeetingTime = 0;
+        MeetingTime = Convert.ToInt32(Console.ReadLine()) - 1;
 
+        if (MeetingTime <= 4)
+        {
+            Console.WriteLine("You choose number " + Times[MeetingTime] + " combination for your book club meeting.");
+        }
+        else
+        {
+            Console.WriteLine($"{MeetingTime} is an meeting combination that does not exist.");
+        }
 
+        Console.WriteLine("Where would you like to travel?");
+        Console.WriteLine("");
+        Console.WriteLine("Choose one of these...");
+        Console.WriteLine("");
 
-        //int[] numArray = new int[5];
-        //numArray[0] = 5;
-        //numArray[1] = 2;
-        //numArray[2] = 10;
-        //numArray[3] = 200;
-        //numArray[4] = 5000;
+        string[] names = { "Paris", "Brasil", "New York", "Rome", "Sidney", "Tokio"};
 
-        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+        Console.WriteLine(names[0]);
+        Console.WriteLine(names[1]);
+        Console.WriteLine(names[2]);
+        Console.WriteLine(names[3]);
+        Console.WriteLine(names[4]);
+        Console.WriteLine(names[5]);
+        Console.WriteLine("");
 
+        int TownName = 0;
+        TownName = Convert.ToInt32(Console.ReadLine()) - 1;
+        Console.WriteLine("You choose the " + names[TownName] + ".");
         Console.ReadLine();
     }
 }
