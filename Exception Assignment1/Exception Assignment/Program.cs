ae4e2a10cdc93ac3ab41e82d4e40696c164d2717
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-08 23:26:47 +0100

    Add files via upload

--- Exception Assignment1/Exception Assignment/Program.cs
+++ Exception Assignment1/Exception Assignment/Program.cs
@@ -17,19 +17,12 @@ class Program
         try
         {
             Console.WriteLine("Type an number to divide each number in the list.");
-            int result = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Pick a second number.");
-            //int numberTwo = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Dividing the two...");
-            //int numberThree = numberOne / numberTwo;
-            //Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
-            //Console.ReadLine();
-
+            int num = Convert.ToInt32(Console.ReadLine());
+           
             foreach(int item in intList)
             {
-               // int result1 = item / intList[0];
-
-                int result1 = item / result;
+                int result = item / num ;
+                Console.WriteLine("The amount is: " + result);
             }
 
         }
