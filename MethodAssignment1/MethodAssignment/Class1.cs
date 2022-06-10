c3254dab45e39489198599cf365de5fca2a14434
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-10 22:39:15 +0100

    Add files via upload

---
+++ MethodAssignment1/MethodAssignment/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+     public class Class1
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Multiply(int data, int value)
+        {
+            return data * value;
+        }
+
+        public int Subtract(int data, int value)
+        {
+            return data - value;
+        }    
+        
+    }
+}
---
+++ MethodAssignment1/MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Program
+    {
+       // public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an number to do some basic math operation.");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            int obj = new Program();
+            int result = obj.Add(userValue, 4);
+            result = obj.Multiply(result, 1);
+            result = obj.Substract(result, 2);
+
+            //int programObject = new Program();
+            //int result = programObject.Add(userValue, 4);
+            //result = programObject.Multiply(result, 1);
+            //result = programObject.Substract(result, 2);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
