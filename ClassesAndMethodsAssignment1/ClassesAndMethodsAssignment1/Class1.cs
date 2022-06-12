f5c95e41dc4b5a3cd116ad99123ada5fe543924d
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-13 00:43:34 +0100

    Add files via upload

---
+++ ClassesAndMethodsAssignment1/ClassesAndMethodsAssignment1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class Class1
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+    //public class Class1
+    //{
+        public decimal Subtract(decimal num3, decimal num4)
+        {
+            return num3 - num4;
+        }
+
+    //}
+
+    //public class Class1
+    //}
+        public int Division (int num5, int num6)
+        { 
+            return num5 / num6;
+        }
+
+        public void GetResults()
+        {
+            Console.WriteLine("y = " + this.Add + "\n" +
+                "y1 = " + this.Subtract + "\n" +
+                "y3 = " + this.Division + "\n");
+
+        }
+
+    }
+
+}   
+
---
+++ ClassesAndMethodsAssignment1/ClassesAndMethodsAssignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class Program
+    {
+        // public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x + 5 = y. Enter the x value.");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj = new Class1();    //makes an object of Class1, object instatiation
+            int result = obj.Add(userValue, 5);
+            Class1 obj1 = obj.Subtract(userValue, 2,45);
+            Class1 obj3 = obj.Division(userValue, 1);
+            //result = obj.Multiply(result, 1);
+            //result = obj.Subtract(result, 2);
+
+            Add.GetResults();
+            Subtract.getResults();
+            Division.getResults();
+        
+
+            Console.ReadLine();
+        }
+    }
+}
