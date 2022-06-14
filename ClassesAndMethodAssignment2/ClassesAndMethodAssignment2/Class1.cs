c794f9607bff7a22e90c020d57d61eb619e6fc53
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-14 10:29:43 +0100

    Add files via upload

---
+++ ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment2
+{
+    public class Class1
+    {
+        public int Add1(int x, int y, int [] n)
+        { 
+            return x + y;
+        }
+
+        //public int Add2(int x, int optionalint = 10)
+        //{
+        //    return x + optionalint;
+        //}
+    }
+}
---
+++ ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment2
+{
+    class Program
+    {
+        static public void Main()
+        //static public void result(int x, int y, int[] n)
+        {
+           
+            Console.WriteLine("We have an simple equation: x + y.");
+            Console.WriteLine("Enter the x value.");
+            int userValueX = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Enter the y value. You may also choose to not enter anything for the value y.");
+           // OptionalMethodWithDefaultValue(10);
+            int userValueY = Convert.ToInt32(Console.ReadLine());
+     
+            Class1 obj = new Class1();    //makes an object of Class1, object instatiation
+            int result = obj.Add(userValueX, userValueY );
+            //int result1 = obj.Add2(userValueY, n );
+
+            foreach ( int no in n)
+            {
+                result += no;
+            }
+
+
+            Console.WriteLine("Result is: {0}", result);
+            Console.ReadLine();
+            
+        }
+    }
+}
