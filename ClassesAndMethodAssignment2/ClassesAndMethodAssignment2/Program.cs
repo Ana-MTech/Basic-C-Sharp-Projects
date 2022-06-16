82c37eaf2e0d08a6b78587dadcc5faa5e36f34b3
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-16 17:43:58 +0100

    Add files via upload

--- ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Program.cs
+++ ClassesAndMethodAssignment2/ClassesAndMethodAssignment2/Program.cs
@@ -13,29 +13,42 @@ namespace ClassesAndMethodAssignment2
             Class1 obj = new Class1();    //makes an object of Class1, object instatiation
 
             Console.WriteLine("We have an simple equation: x + y.");
-            Console.WriteLine("Enter the x value.");
+            Console.WriteLine("Enter the x value, an positive whole number");
             int userValueX = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the y value. You may also choose to not enter anything for the value y.");
-            string userValueY = Console.ReadLine();  //make this variable string and use ReadLine to get user input
-             
+            //Console.WriteLine("Enter the y value. You may also choose to not enter anything for the value y.");
+            //string userValueY = Console.ReadLine();  //make this variable string and use ReadLine to get user input
 
-            if (userValueY == " ") //use '=='for comparision, '='assigns value
-            {
-                int result = obj.Add(userValueX);
-                Console.WriteLine(userValueX);
-            }
 
-            else               
+            //if (userValueY == " ") //use '=='for comparision, '='assigns value
+            //{
+            //    int result = obj.Add(userValueX);
+
+            //}
+
+            //else               
+            //{
+            //    int userValueZ = Convert.ToInt32(userValueY); //new variable set equal to the converted userValueY
+            //    int result1 = obj.Add(userValueX, userValueZ);
+
+            //}
+
+            try 
             {
-                int userValueZ = Convert.ToInt32(userValueY); //new variable set equal to the converted userValueY
-                int result1 = obj.Add(userValueX, userValueZ);
-                Console.WriteLine(userValueX + userValueZ);
-               
+                Console.WriteLine("Enter the y value. You may also choose to not enter anything for the value y.");
+                int userValueY = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Your x and y value added together equal" + obj.Add(userValueX, userValueY)); 
             }
-                       
-            Console.ReadLine();
-            
+            catch (FormatException)
+            {
+                Console.WriteLine("Your only number choosn is " + userValueX);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+     
         }
     }
 }
