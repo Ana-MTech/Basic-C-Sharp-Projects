b1cbfeb6aa1c3e1261f47d2fc4554739a39f21c7
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-18 01:39:19 +0100

    Add files via upload

---
+++ ClassesAndMethodAssignment2/ClassesAndMethodsAssignment1/ClassesAndMethodsAssignment1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class Class1
+    {
+        public int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public decimal Subtract(decimal num3, decimal num4)
+        {
+            return num3 - num4;
+        }
+
+        public int Division (int num5, int num6)
+        { 
+            return num5 / num6;
+        }
+
+        public int stringMethod (string num7)  // takes in a string as the parametar
+        {
+            int num8 = Convert.ToInt32(num7);
+            return num8;   // returns the integer
+        }
+    }
+
+}   
+
---
+++ ClassesAndMethodAssignment2/ClassesAndMethodsAssignment1/ClassesAndMethodsAssignment1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("x + 5 = y. Enter the x value.");
+            //int userValue = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj = new Class1();    //makes an object of Class1, object instatiation
+
+            //get first number and call first method
+            Console.WriteLine("x + 5 = y. Enter the x value in order to see result");
+            
+            //Console.WriteLine("Enter a number.");
+            int UserValue = Convert.ToInt32(Console.ReadLine());
+            int result = obj.Add(UserValue, 5);
+
+            //get second number and call first method
+            Console.WriteLine("x - 2.45 = y. Enter the x value in order to see result");
+
+            //Console.WriteLine("Enter another number.");
+            int UserValue2 = Convert.ToInt32(Console.ReadLine());
+            decimal result2 = obj.Subtract(UserValue2, 2.45m);
+
+            //get third number and call first method
+            Console.WriteLine("x : 1 = y. Enter the x value in order to see result");
+
+            //Console.WriteLine("Enter a third number.");
+            int UserValue3 = Convert.ToInt32(Console.ReadLine());
+            int result3 = obj.Division(UserValue3, 1);
+
+            //get fourth number,but save as a string, and call fourth method          
+            Console.WriteLine("Enter a fourth number.");
+            string UserValue4 = Console.ReadLine();
+            int result4 = obj.stringMethod(UserValue4);
+
+            //here we use Console.WriteLine to return the result of the each method
+
+            Console.WriteLine("The result of the first method is: " + result);
+            Console.WriteLine("The result of the second method is: " + result2);
+            Console.WriteLine("The result of the third method is: " + result3);
+            Console.WriteLine("The result of the fourth method is: " + result4);
+
+            Console.ReadLine();
+        }
+    }
+}
