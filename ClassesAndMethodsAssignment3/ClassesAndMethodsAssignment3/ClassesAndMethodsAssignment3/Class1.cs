7c86bb6f1f591a46748653716fe407b7db5c8ae7
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-20 17:55:33 +0100

    Add files via upload

---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Class1
+    {
+        public void Add( int firstNumber, int secondNumber)
+
+        {
+            int total = firstNumber + secondNumber;   // creating variable to hold the value of the sum of two parmetars
+            Console.WriteLine(total);   //  use Console.WriteLine to returm total value on screen
+        }
+       
+    }
+}
---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Class1 obj = new Class1();
+
+            obj.Add(firstNumber: 123, secondNumber: 55);
+
+            int n = 5;
+            int n1 = 2;
+
+            obj.Add(n, n1);
+            //Console.WriteLine("The value: {0}", n + n1);
+            Console.ReadLine();
+       
+        }
+
+    }
+
+}
