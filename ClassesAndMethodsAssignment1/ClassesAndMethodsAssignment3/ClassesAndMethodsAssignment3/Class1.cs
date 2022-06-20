105e2103d80c210bf24ae384d0571aa6c30f940b
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-20 01:23:14 +0100

    Add files via upload

---
+++ ClassesAndMethodsAssignment1/ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Class1
+    {
+        public void Add( int firstNumber, int secondNumber)
+
+        {
+            Add(firstNumber, secondNumber);
+      
+        }
+       
+    }
+}
