a79dac7355c833f35341fe7a0e0d150eb68778f8
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-20 01:25:14 +0100

    Add files via upload

---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Class1 obj = new Class1();
+
+            //Console.WriteLine(Add(firstNumber: 123, secondNumber: 55));
+            Console.WriteLine(firstNumber: 123, secondNumber: 55);
+
+
+            Console.ReadLine();
+       
+        }
+
+    }
+
+}
