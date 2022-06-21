257f84e79caa29c5291a9f2b28e05dabe84be5fb
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-21 23:55:16 +0100

    Add files via upload

---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+
+            obj.FirstName = "Sample";
+            Console.WriteLine("FirstName");
+
+            obj.LastName = "Student";
+            Console.WriteLine("LastName");
+
+            //call the superclass method SayName() on the Employee object
+            Employee person = new Employee();
+            person.SayName();
+
+              
+            Console.ReadLine();
+        }
+    }
+}
