4bb06303ab33aba48cc97154c82e49ab4ee7e820
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-21 07:52:55 +0100

    Add files via upload

---
+++ ClassMethodAssgnment/ClassMethodAssgnment/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Class1
+    {
+        public static void Division(int x )
+        {
+             {
+                int total = x / 2;  
+                Console.WriteLine(total);   
+             }
+
+        }
+
+        public static void Division1(int y, int z)
+
+        {
+            int total = y / z;   
+            Console.WriteLine(total);   
+        }
+    }
+}
---
+++ ClassMethodAssgnment/ClassMethodAssgnment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Class1 obj = new Class1();
+
+                Console.WriteLine("x : 2 = y");
+                Console.WriteLine("Enter the x value, an positive whole number");
+                int userValue = Convert.ToInt32(Console.ReadLine());
+                obj.Division(userValue / 2);
+
+                Console.WriteLine("10 / 5:");
+                int y = 10;
+                int z = 5;
+                obj.Division1( y / z );
+               
+
+            Console.ReadLine();
+
+        }
+    }
+}
