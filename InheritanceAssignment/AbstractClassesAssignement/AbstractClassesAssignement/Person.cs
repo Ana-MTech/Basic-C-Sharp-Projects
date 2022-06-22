831a6a4b267b32ecbfd475029be55f5f9ab46b21
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-22 19:01:20 +0100

    Add files via upload

---
+++ InheritanceAssignment/AbstractClassesAssignement/AbstractClassesAssignement/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignement
+{
+    public abstract class Person
+    {
+        public string firstName
+        {
+            get;
+            set;
+        }
+
+        public string lastName
+        {
+            get;
+            set;
+        }
+      
+        public void SayName()
+        {
+            Console.WriteLine("Name: [full name]");
+        }
+    }
+}
