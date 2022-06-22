1527b714bb268fe2aac8a1788755b3e60a10aa70
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-22 19:03:43 +0100

    Add files via upload

---
+++ AbstractClassesAssignement/AbstractClassesAssignement/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignement
+{
+    public class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Ema Smith");
+         }
+
+
+    }
+}
---
+++ AbstractClassesAssignement/AbstractClassesAssignement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+
+            person.firstName = "Sample";
+            Console.WriteLine("firstName");
+
+            person.lastName = "Student";
+            Console.WriteLine("lastName");
+
+            person.SayName();      
+           
+            Console.ReadLine();
+        }
+    }
+}
