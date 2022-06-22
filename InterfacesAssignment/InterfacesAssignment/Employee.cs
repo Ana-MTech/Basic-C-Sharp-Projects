13606da9bcb9f2e6bdc2146e20de6e96dfec54c5
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-23 00:57:47 +0100

    Add files via upload

---
+++ InterfacesAssignment/InterfacesAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+        public class Employee : IQuittable
+        {
+            public void SayName()
+            {
+                Console.WriteLine(firstName + "" + lastName);
+            }
+
+            public void Quit()
+            {
+                 Console.WriteLine("The list for first quartal.");
+            }
+    } 
+}
---
+++ InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable file1 = new Employee();
+            Employee file2 = new Employee();
+
+
+            //file2.firstName = "Name";
+            //file2.lastName = "Surname";
+
+            file2.SayName();
+            file1.Quit();
+            Console.ReadLine();
+        }
+    }
+}
