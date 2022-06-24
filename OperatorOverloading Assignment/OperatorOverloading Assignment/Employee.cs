797031497086f8f76061ec40964223fef87490c9
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-24 01:07:14 +0100

    Add files via upload

---
+++ OperatorOverloading Assignment/OperatorOverloading Assignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading_Assignment
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID != employee2.ID)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
---
+++ OperatorOverloading Assignment/OperatorOverloading Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ID1 = new Employee();
+
+            ID1.FirstName = "Mary";
+            ID1.LastName = "Smith";
+            ID1.ID = 23487651;
+
+            Employee ID2 = new Employee();
+
+            ID2.FirstName = "Patrick";
+            ID2.LastName = "Shea";
+            ID2.ID = 55556327;
+
+            if (ID1 == ID2)
+                Console.WriteLine("Same ID");
+            else
+                Console.WriteLine(""ifferent ID);
+
+            Console.ReadLine();
+        }
+    }
+}
