ea00c5bdc447e3f4710c7b210fde69fecb648774
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-25 23:18:45 +0100

    Add files via upload

---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What day of the week you workout the most?");
+                //check if string user input exist in the enum Weekday
+                Weekday day = (Weekday)Enum.Parse(typeof(Weekday), Console.ReadLine());
+                //Print result 
+                Console.WriteLine("You workout mostly on " + day);
+            }
+            //If no proper weekday input, display error message.
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+    //enum class Weekday
+    public enum Weekday
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
