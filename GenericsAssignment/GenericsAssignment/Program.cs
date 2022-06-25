c9e29643bff42e4259a170d659797d53804df61d
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-25 21:08:25 +0100

    Add files via upload

---
+++ GenericsAssignment/GenericsAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee<string> Things1 = new Employee<string>();
+  
+            Employee<int> Things2 = new Employee<int>();
+ 
+            Things1.things = new List<string>() { "John Willis", "Tom Perry", "Any Smith" };
+
+            Things2.things = new List<int>() { 1, 2, 3 };
+
+            foreach (string things in Things1.things)
+            {
+                Console.WriteLine(things);
+            }
+
+            foreach (int things in Things2.things)
+            {
+                Console.WriteLine(things);
+            }
+            Console.ReadLine();
+        }
+    }
+}
