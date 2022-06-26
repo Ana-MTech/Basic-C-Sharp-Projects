f84babe51c5a125dbe2d1d292e8d8dce27ac274e
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-26 22:33:55 +0100

    Add files via upload

---
+++ StructsAssignement/StructsAssignement/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignement
+{
+    struct Number
+
+    public struct Number
+    {
+        public decimal Amount1;
+        public decimal Amount2;
+    }
+
+}
+
---
+++ StructsAssignement/StructsAssignement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number dec;
+            dec.Amount1 = 3.01m;
+            dec.Amount2 = 4.01m;
+
+            Console.ReadLine();
+
+        }
+    }
+}
