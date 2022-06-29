6be567442df238a51127ecd8e9f12d02475750a1
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-29 17:00:05 +0100

    Add files via upload

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The currant date and time: " + DateTime.Now);
+
+            Console.WriteLine("Enter a number of hours that will be added on time now.");
+            int passHours = Int32.Parse(Console.ReadLine());
+
+            //data and time after the passHours has bneen added to DateTime.Now
+            Console.WriteLine("In " + passHours + " it will be " + DateTime.Now.AddHours(passHours));
+            Console.ReadLine();
+        }
+    }
+}
