b2cda277e10c20cd7ddca811cd9c5fe904b8b253
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-30 00:09:27 +0100

    Add files via upload

---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string theName = "Name";
+
+            Console.WriteLine("Welcome, {0}.", theName);
+
+            //var newAddition = new Addition("10");
+
+            var Int = 18;
+            if (Int > 18 )
+            {
+                Console.WriteLine(" You can have your driving licence" + theName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
