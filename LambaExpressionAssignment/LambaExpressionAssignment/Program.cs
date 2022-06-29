fd456ad839af535cf5023232e21a463075bd3026
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-29 10:50:29 +0100

    Add files via upload

--- LambaExpressionAssignment/LambaExpressionAssignment/Program.cs
+++ LambaExpressionAssignment/LambaExpressionAssignment/Program.cs
@@ -27,54 +27,28 @@ namespace LambaExpressionAssignment
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
             };
 
+            //count of employee.firstName string that are "Joe"
+            int count = employee.firstName.Count(x => x == "Joe");
+
+            //create list > joeList from employee.firstName that contain "Joe"
             List<string> joeList = employee.firstName.Where(x => x == "Joe").ToList();
 
+            //create a list from employee.ID where ID is above 5
+            List<int> aboveFive = employee.ID.Where(x => x > 5).ToList();
+
+            //loop that prints a list of employee data from the list aboveFive
+            for (int i = aboveFive[0] - 1; i < aboveFive[0] + aboveFive.Count - 1; i++)
+            {
+                Console.WriteLine(employee.ID[i] + " " + employee.firstName[i] + " " + employee.lastName[i]);
+            }
+
+            //loop that prints a list of employee data from the list joeList
             foreach (string joe in joeList)
             {
                 Console.WriteLine("\n"+joe);
             }
 
-            List<int> aboveFive = employee.ID.Where(x => x > 5).ToList();
-
-
-
-            //List<Employee> Employees = new List<Employee>();
-            //List<string> firstNames = new List<string>()
-            //{
-            //    "Ann", "Joe", "Jason", "Ema", "Luke", "Math", "Joe", "Mary", "Rob", "Lee"
-            //};
-
-            //List<string> lastNames = new List<string>()
-            //{
-            //    "Smith", "Jones", "Taylor", "Brown", "Wilson", "Johnson", "Davies", "Cooper", "Lewis", "Wright"
-            //};
-
-            //List<int> IDs = new List<int>()
-            //{
-            //    3456542, 2347649, 8509003, 28394755, 23459584, 39303495, 39393903, 94857857, 9383833, 83700213
-            //};
-
-            //List<int> indexes = new List<int>()
-            //{
-            //    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            //};
-
-            ////foreach (int index in indexes)
-            //for (int i = 0; < firstNames.Length; i++)
-            //{
-            //    Employee Emplojeeobj = new Employee();
-            //    Emplojeeobj.firstName = firstNames[i];
-            //    Emplojeeobj.lastName = lastNames[i];
-            //    Emplojeeobj.ID = IDs[i];
-            //    Employees.Add(Emplojeeobj);
-            //}
-
-            //foreach (Employee Employee in Employees)
-            //{
-            //    Console.WriteLine(Employee.firstName + " " + Employee.lastName + " " + Employee.ID);
-
-            //}
-
+            Console.ReadLine();
 
             //List<Employee> listEmployees = new List<Employee>();
             //listEmployees.Add(new Employee("John", "Taylor", 1));
@@ -87,13 +61,6 @@ namespace LambaExpressionAssignment
             //listEmployees.Add(new Employee("Mary", "Cooper", 8));
             //listEmployees.Add(new Employee("Rob", "Lewis", 9));
             //listEmployees.Add(new Employee("Lee", "Wright", 10));
-
-            //foreach (Employee employee in listEmployees.FindAll(x => (x.Name == "Joe")).ToList()) ;
-            //{
-            //    Console.WriteLine(Employee.Name + Employee.Surname);
-            //}
-
-           Console.ReadLine();
 
         }
     }
