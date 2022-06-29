29d0e35d73ba19f8814aa940420e71e7614ab4d9
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-29 15:27:52 +0100

    Add files via upload

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\C# and NET.Framework\test.txt";
+
+            Console.WriteLine("Enter a number.");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(userValue)
+
+
+            using (var file = new StreamWriter(filePath, true))
+            {
+
+                file.WriteLine(userValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
