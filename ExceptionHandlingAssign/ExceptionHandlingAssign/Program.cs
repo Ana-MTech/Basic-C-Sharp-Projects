aa804408c1f43a8b671c9fc01947c1c64f751fff
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-06-30 18:58:33 +0100

    Add files via upload

--- ExceptionHandlingAssign/ExceptionHandlingAssign/Program.cs
+++ ExceptionHandlingAssign/ExceptionHandlingAssign/Program.cs
@@ -10,6 +10,37 @@ namespace ExceptionHandlingAssign
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Please, enter your age.");
+                int yourAge = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("You were born before " + yourAge + " and your birth year is " + DateTime.Today.AddYears(-yourAge).ToString("yyyy"));
+
+                bool validAnswer = false;
+                int age = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Please, enter your age.");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter whole number only, no null or negative values.");
+                }
+
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter an whole number. The null and negative values are not allowd.");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured. please contact your System Administrator.");
+            }
+            finally  //finally Readline to keep program open even if program ends prematurely
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
--- ExceptionHandlingAssign/Program.cs
+++ ExceptionHandlingAssign/Program.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExceptionHandlingAssign
+namespace ExceptionHandlingAssignment
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, enter your age.");
+            string yourAge = Console.ReadLine();
+
             try
             {
-                Console.WriteLine("Please, enter your age.");
-                int yourAge = Int32.Parse(Console.ReadLine());
+                int yourAge1 = Int32.Parse(yourAge);
 
-                Console.WriteLine("You were born before " + yourAge + " and your birth year is " + DateTime.Today.AddYears(-yourAge).ToString("yyyy"));
+                Console.WriteLine("You were born before " + yourAge + " and your birth year is " + DateTime.Today.AddYears(-yourAge1).ToString("yyyy"));
 
                 bool validAnswer = false;
                 int age = 0;
@@ -25,22 +27,49 @@ namespace ExceptionHandlingAssign
                     validAnswer = int.TryParse(Console.ReadLine(), out age);
                     if (!validAnswer) Console.WriteLine("Please enter whole number only, no null or negative values.");
                 }
-
             }
 
-            catch (FormatException)
-            {
-                Console.WriteLine("Please enter an whole number. The null and negative values are not allowd.");
-            }
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Please enter an whole number. The null and negative values are not allowd.");
+            //}
 
-            catch (Exception)
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("An error occured. please contact your System Administrator.");
+            //}
+            //finally  //finally Readline to keep program open even if program ends prematurely
+            //{
+            //    Console.ReadLine();
+            //}
+        class NegativeNumberNotAllowed : Exception
+        {
+            public NegativeNumberNotAllowed(string message) : base(message)
             {
-                Console.WriteLine("An error occured. please contact your System Administrator.");
-            }
-            finally  //finally Readline to keep program open even if program ends prematurely
-            {
-                Console.ReadLine();
+
             }
         }
+
+        class Test
+        {
+            static void CheckForNegative(int number)
+            {
+                if (number < 0)
+                {
+                    throw new NegativeNumberNotAllowed("Negative numer is no allowed.");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+            try
+            }
+            CheckForNegative(-10);
+            }
+            catch(NegativeNumberNotAllowed e)
+        }
+        Console.WriteLine(e);
+        }
+
     }
 }
