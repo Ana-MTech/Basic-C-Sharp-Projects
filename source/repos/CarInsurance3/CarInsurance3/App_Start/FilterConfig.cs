0ed55a3f05011ddb9d84c1c006a13ca0249a18e0
Ana-MTech <anamaria137@mail.com>
2022-07-16 16:23:51 +0100

    C:UsersMyPCsourcereposCarInsurance3

---
+++ source/repos/CarInsurance3/CarInsurance3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ source/repos/CarInsurance3/CarInsurance3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarInsurance3.MvcApplication" Language="C#" %>
